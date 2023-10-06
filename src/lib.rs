07a0fc26b6ef124af019d7a65f4ef42e303a6c5d
dachus <dachus@dachus>
2023-10-06 14:33:34 +0200

    indexing code not tested

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,14 @@ use alloy_primitives::FixedBytes;
 use alloy_sol_types::{sol, SolEvent};
 use bindings::component::uq_process::types::*;
 use bindings::{
-    get_payload, print_to_terminal, receive, send_and_await_response, send_request, send_requests,
-    send_response, Guest,
+    get_payload, print_to_terminal, receive, send_request,
+    Guest,
 };
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::collections::{HashMap, HashSet};
 
-// mod process_lib;
+mod process_lib;
 
 struct Component;
 
@@ -34,13 +35,29 @@ struct EthEvent {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-struct State {
-    // namehash to human readable name
-    // allSecrets: HashMap<String, String>,
-    // mySecrets: HashMap<String, String>,
-    // myBids: HashMap<String, String>,
+struct Bid {
+    from: String, // .uq name
+    amount: String,
+    messageHash: String,
+    time: u64,
+}
 
-    // last block we read from
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Secret {
+    from: String, // .uq name
+    message: String,
+    messageHash: String,
+    topBid: Option<Bid>,
+    secret: Option<String>,
+    time: u64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct State {
+    secrets: HashMap<String, Secret>, // NOTE order these by time
+    mySecrets: HashMap<String, String>,
+    myBids: HashSet<String>,
+    // last block we read from TODO might not need this
     block: u64,
 }
 
@@ -75,6 +92,28 @@ impl Guest for Component {
     fn init(our: Address) {
         print_to_terminal(0, "secrets: start");
 
+        let mut state = State {
+            secrets: HashMap::new(),
+            mySecrets: HashMap::new(),
+            myBids: HashSet::new(),
+            block: 1,
+        };
+
+        match process_lib::get_state(our.node.clone()) {
+            None => {},
+            Some(p) => {
+                match bincode::deserialize::<State>(&p.bytes) {
+                    Err(e) => print_to_terminal(
+                        0,
+                        &format!("qns_indexer: failed to deserialize payload from fs: {}", e),
+                    ),
+                    Ok(s) => {
+                        state = s;
+                    },
+                }
+            },
+        }
+
         let _subscribe_to_events_res = send_request(
             &Address{
                 node: our.node.clone(),
@@ -85,31 +124,31 @@ impl Guest for Component {
                 expects_response: Some(5), // TODO evaluate
                 metadata: None,
                 // -1 because there could be other events in the last processed block
-                ipc: Some(subscribe_to_secrets(0)), // TODO
+                ipc: Some(subscribe_to_secrets(state.block - 1)),
             },
             None,
             None,
         );
 
-        // let _bind_app_res = send_request(
-        //     &Address{
-        //         node: our.node.clone(),
-        //         process: ProcessId::Name("http_bindings".to_string()),
-        //     },
-        //     &Request{
-        //         inherit: false,
-        //         expects_response: None,
-        //         metadata: None,
-        //         ipc: Some(serde_json::json!({
-        //             "action": "bind-app",
-        //             "path": "/secrets",
-        //             "app": "secrets",
-        //             "authenticated": true,
-        //         }).to_string()),
-        //     },
-        //     None,
-        //     None,
-        // );
+        let _bind_app_res = send_request(
+            &Address{
+                node: our.node.clone(),
+                process: ProcessId::Name("http_bindings".to_string()),
+            },
+            &Request{
+                inherit: false,
+                expects_response: None,
+                metadata: None,
+                ipc: Some(serde_json::json!({
+                    "action": "bind-app",
+                    "path": "/secrets",
+                    "app": "secrets",
+                    "authenticated": true,
+                }).to_string()),
+            },
+            None,
+            None,
+        );
 
         loop {
             let Ok((source, message)) = receive() else {
@@ -131,12 +170,51 @@ impl Guest for Component {
                     match decode_hex(&e.topics[0].clone()) {
                         NewSecret::SIGNATURE_HASH => {
                             print_to_terminal(0, "got new secret");
+                            let message_hash       = &e.topics[1];
+                            let decoded    = NewSecret::decode_data(&decode_hex_to_vec(&e.data), true).unwrap();
+                            let message = decoded.0;
+                            let name = dnswire_decode(decoded.1);
+                            let time = e.blockNumber.parse::<u64>().unwrap();
+
+                            let secret = Secret {
+                                from: name,
+                                message: message,
+                                messageHash: message_hash.clone(),
+                                topBid: None,
+                                secret: None,
+                                time: time,
+                            };
+
+                            state.secrets.insert(message_hash.clone(), secret);
                         }
                         BidPlaced::SIGNATURE_HASH => {
                             print_to_terminal(0, "new bid placed");
+                            let message_hash    = &e.topics[1];
+                            let from           = &e.topics[2];
+                            let decoded    = BidPlaced::decode_data(&decode_hex_to_vec(&e.data), true).unwrap();
+                            let amount = decoded.0;
+                            let name = dnswire_decode(decoded.1);
+
+                            let bid = Bid {
+                                from: name,
+                                amount: amount.to_string(),
+                                messageHash: message_hash.clone(),
+                                time: e.blockNumber.parse::<u64>().unwrap(),
+                            };
+
+                            let secret = state.secrets.get_mut(&message_hash.clone()).unwrap();
+                            secret.topBid = Some(bid);
                         }
                         SecretRevealed::SIGNATURE_HASH => {
                             print_to_terminal(0, "secret revealed");
+                            let message_hash   = &e.topics[1];
+                            let who           = &e.topics[2];
+                            let decoded    = SecretRevealed::decode_data(&decode_hex_to_vec(&e.data), true).unwrap();
+                            let revealed_message = decoded.0;
+                            let name = dnswire_decode(decoded.1);
+
+                            let secret = state.secrets.get_mut(&message_hash.clone()).unwrap();
+                            secret.secret = Some(revealed_message);
                         }
                         event => {
                             bindings::print_to_terminal(0, format!("qns_indexer: got unknown event: {:?}", event).as_str());
@@ -164,4 +242,41 @@ fn decode_hex(s: &str) -> FixedBytes<32> {
     let mut arr = [0_u8; 32];
     arr.copy_from_slice(&hex::decode(hex_part).unwrap()[0..32]);
     FixedBytes(arr)
+}
+
+fn decode_hex_to_vec(s: &str) -> Vec<u8> {
+    // If the string starts with "0x", skip the prefix
+    let hex_part = if s.starts_with("0x") {
+        &s[2..]
+    } else {
+        s
+    };
+
+    hex::decode(hex_part).unwrap()
+}
+
+fn dnswire_decode(wire_format_bytes: Vec<u8>) -> String {
+    let mut i = 0;
+    let mut result = Vec::new();
+
+    while i < wire_format_bytes.len() {
+        let len = wire_format_bytes[i] as usize;
+        if len == 0 { break; }
+        let end = i + len + 1;
+        let mut span = wire_format_bytes[i+1..end].to_vec();
+        span.push('.' as u8);
+        result.push(span);
+        i = end;
+    };
+
+    let flat: Vec<_> = result.into_iter().flatten().collect();
+
+    let name = String::from_utf8(flat).unwrap();
+
+    // Remove the trailing '.' if it exists (it should always exist)
+    if name.ends_with('.') {
+        name[0..name.len()-1].to_string()
+    } else {
+        name
+    }
 }
